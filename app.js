5b99105630ee4b70e17301a6c1e99da99da89aa7
Ramakeerthi <ramakeerthi09@gmail.com>
2024-09-15 23:29:24 +0530

    Updated express basics

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
-const http = require('http');
+const express = require('express');
 
-const routes = require('./routes');
+const app = express();
 
-console.log(routes.someText);
+app.use('/', (req,res,next) =>{
+    console.log('This always runs');
+    next();
+});
 
-const server = http.createServer(routes.handler);
+app.use('/add-product', (req, res, next) => {
+    console.log('In another middleware');
+    //If not calling next middleware we need to send response
+    res.send('<h1>The Add Product page!</h1>');
+});
 
-server.listen(3000);
+app.use('/', (req, res, next) => {
+    console.log('In another middleware');
+    //If not calling next middleware we need to send response
+    res.send('<h1>Welcome to express</h1>');
+});
+
+app.listen(3000);
