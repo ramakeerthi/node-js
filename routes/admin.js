6aaf8d90bd52c50d70bc460e0a5853c2f1ffdfbd
Ramakeerthi <ramakeerthi09@gmail.com>
2024-09-16 21:52:20 +0530

    Adding html basic file navigation without utils

--- routes/admin.js
+++ routes/admin.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const express = require('express');
 
 const router = express.Router();
@@ -5,10 +7,10 @@ const router = express.Router();
 router.get('/add-product', (req, res, next) => {
     // console.log('In another middleware');
     //If not calling next middleware we need to send response
-    res.send('<form action="/admin/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
 });
 
-router.post('/product',(req, res, next) => {
+router.post('/add-product',(req, res, next) => {
     console.log(req.body);
     res.redirect('/');
 });
--- routes/shop.js
+++ routes/shop.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const express = require('express');
 
 const router = express.Router();
@@ -5,7 +7,7 @@ const router = express.Router();
 router.get('/', (req, res, next) => {
     // console.log('In another middleware');
     //If not calling next middleware we need to send response
-    res.send('<h1>Welcome to express</h1>');
+    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
 });
 
 module.exports = router;
