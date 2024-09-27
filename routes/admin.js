cb492507ff4ea51fc7b41612e69e6d3c3895bdbb
Ramakeerthi <ramakeerthi09@gmail.com>
2024-09-27 18:12:02 +0530

    Added additional EJS placeholders, routes

--- routes/admin.js
+++ routes/admin.js
@@ -6,10 +6,10 @@ const productsController = require('../controllers/products')
 
 const router = express.Router();
 
-// /admin/add-product => GET
 router.get('/add-product', productsController.getAddProduct);
 
-// /admin/add-product => POST
 router.post('/add-product', productsController.postAddProduct);
+
+router.get('/products');
 
 module.exports = router;
