1d65eecd6d99dc47a4ba73741a3e42d1d3f64ba8
Ramakeerthi <ramakeerthi09@gmail.com>
2024-09-29 17:16:48 +0530

    added edit product

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product');
+const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
     Product.fetchAll((products) => {
@@ -7,6 +8,17 @@ exports.getProducts = (req, res, next) => {
             path: '/products',
             pageTitle: 'All Products'
         });
+    });
+}
+
+exports.getProduct = (req, res, next) => {
+    const prodId = req.params.productId;
+    Product.findById(prodId, product=>{
+        res.render('shop/product-detail', {
+            product: product,
+            pageTitle: "Product Detail",
+            path: '/products'
+        })
     });
 }
 
@@ -25,6 +37,14 @@ exports.getCart = (req, res, next) => {
         path: '/cart',
         pageTitle: 'Your Cart'
     });
+}
+
+exports.postCart = (req, res, next) => {
+    const prodId = req.body.productId;
+    Product.findById(prodId, (product) => {
+        Cart.addProduct(prodId, product.price);
+    });
+    res.redirect('/cart');
 }
 
 exports.getOrders = (req, res, next) => {
